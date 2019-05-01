cf9cc5cd64f29ff3745c493081ad75460d8809a5
oOBoomberOo <boombercmd123@gmail.com>
2019-05-02 06:41:02 +0700

    Working

--- src/app.js
+++ src/app.js
@@ -13,6 +13,10 @@ const isLinux = (process.platform === 'linux');
 const workingDirectory = path.join(app.getAppPath('temp'), 'temp');
 const appData = app.getAppPath('appData');
 
+const fsReadDir = promisify(fs.readdir);
+const fsReadFile = promisify(fs.readFile);
+const fsWriteFile = promisify(fs.writeFile);
+
 process.env.ENVIRONMENT = 'development';
 
 let windows = {};
@@ -109,9 +113,11 @@ function addResourcepacks(event, windowName) {
 		],
 		properties: ['openFile', 'multiSelections']
 	}, files => {
-		for (let file of files) {
-			let resourcepack = resolveResourcepack(file);
-			resourcepacks[resourcepack.id] = resourcepack;
+		if (files) {
+			for (let file of files) {
+				let resourcepack = resolveResourcepack(file);
+				resourcepacks[resourcepack.id] = resourcepack;
+			}
 		}
 		event.reply('response:update_resourcepack', resourcepacks);
 	});
@@ -133,8 +139,173 @@ function resolveResourcepack(file) {
 }
 
 async function compileResourcepack(event) {
-	await del([path.join(workingDirectory, '**'), `!${workingDirectory}`]);
-	mkdirp(workingDirectory);
+
+	console.log(`Start compiling resourcepack`);
+	event.reply('response:compile:start');
+
+	const resourceDirectory = path.join(workingDirectory, 'resource');
+	const resultDirectory = path.join(workingDirectory, 'result');
+	await del([path.join(workingDirectory, '**')]);
+	mkdirp.sync(resourceDirectory);
+	mkdirp.sync(resultDirectory);
+
+	console.log(`Start unzipping resourcepacks`);
+	let index = 0;
+	let promises = [];
+	for (let id in resourcepacks) {
+		let resourcepack = resourcepacks[id];
+		let source = resourcepack.path;
+		let target = path.join(resourceDirectory, `${index}`);
+		promises.push(unzipFile(source, target));
+		index++;
+	}
+
+	await Promise.all(promises);
+	promises = [];
+	let data = [];
+	let subdir = await fsReadDir(resourceDirectory);
+	for (let dir of subdir) {
+		let directory = path.join(resourceDirectory, dir);
+		data.push(walkDirectory(directory, directory));
+	}
+
+	let messyList = [];
+	for await (let element of data) {
+		messyList.push(element);
+	}
+
+	console.log(`Start separating files`);
+	let maxProgress = 0;
+	let nonDupeList = {};
+	let dupeList = {};
+	for (let i = 0; i < messyList.length; i++) {
+		let element = messyList[i];
+		for (let id in element) {
+			let item = element[id];
+			let stat = isDuplicate(id, messyList, i);
+			if (stat) {
+				if (!dupeList[id]) {
+					dupeList[id] = [];
+				}
+				dupeList[id].push(item);
+				maxProgress += 1;
+			}
+			else {
+				nonDupeList[id] = item;
+				maxProgress += 1;
+			}
+		}
+	}
+
+	let currentProgress = {i: 0, event: event};
+	event.reply('response:compile:init_length', maxProgress);
+
+	console.log(`Start copying non-duplicate files`);
+	for (let id in nonDupeList) {
+		let file = nonDupeList[id];
+		let source = file.path;
+		let target = source.replace(file.from, resultDirectory);
+		pipeFile(source, target, currentProgress);
+	}
+
+	console.log(`Start comparing duplicate files`);
+	for (let id in dupeList) {
+		let element = dupeList[id];
+		for (let i = 0; i < element.length; i++) {
+			let item = element[i];
+			let source = item.path;
+			let target = source.replace(item.from, resultDirectory);
+			if (i > 0) {
+				compareFile(source, target, currentProgress);
+			}
+			else {
+				pipeFile(source, target, currentProgress);
+			}
+		}
+	}
+}
+
+async function compareFile(source, target, progress) {
+	let extension = getExtension(target);
+	let src_data = fsReadFile(source).catch(error => null);
+	let target_data = fsReadFile(target).catch(error => null);
+	let data = await Promise.all([src_data, target_data]);
+	let source_result, target_result;
+	if (extension === 'json' || extension === 'mcmeta') {
+		source_result = JSON.parse(data[0] == null ? '{}': data[0].toString());
+		target_result = JSON.parse(data[1] == null ? '{}': data[1].toString());
+		temp_data = {};
+		temp_data = Object.assign(source_result, target_result);
+		target_result = JSON.stringify(temp_data, null, 2);
+	}
+	else {
+		if (data[1] === null) {
+			data[1] = data[0];
+		}
+	}
+	await fsWriteFile(target, data[1]).catch(console.trace);
+	progress.i += 1;
+	progress.event.reply('response:compile:update_count', progress.i);
+	console.log(`[${progress.i}] Done comparing file from ${source} to ${target}`);
+}
+
+async function pipeFile(source, target, progress) {
+	let directory = getDirectory(target);
+	mkdirp.sync(directory);
+	let sourceStream = fs.createReadStream(source);
+	let targetStream = fs.createWriteStream(target);
+	sourceStream.pipe(targetStream).on('close', () => {
+		progress.i += 1;
+		progress.event.reply('response:compile:update_count', progress.i);
+		console.log(`[${progress.i}] Done piping file from ${source} to ${target}`);
+	});
+}
+
+function getDirectory(file) {
+	return file.match(/(.*)[\/\\]/g)[0];
+
+}
+
+function isDuplicate(id, data, ownIndex) {
+	for (let i = 0; i < data.length; i++) {
+		if (i !== ownIndex) {
+			let element = data[i];
+			if (id in element) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+async function unzipFile(source, target) {
+	console.log(`Unzipping file from ${source} to ${target}`);
+	mkdirp.sync(target);
+	return fs.createReadStream(source)
+		.pipe(unzipper.Extract({path: target}))
+		.promise();
+}
+
+async function walkDirectory(directory, rootDirectory) {
+	let result = {};
+	let fileStat = promisify(fs.stat);
+	let files = await fsReadDir(directory);
+	for (let file of files) {
+		let filePath = path.join(directory, file);
+		let stat = await fileStat(filePath);
+		if (stat.isDirectory()) {
+			let subdirectory = await walkDirectory(filePath, rootDirectory);
+			for (let id in subdirectory) {
+				let item = subdirectory[id];
+				result[id] = item;
+			}
+		}
+		else {
+			let id = md5(filePath.replace(rootDirectory, ''));
+			result[id] = {path: filePath, from: rootDirectory};
+		}
+	}
+	return result;
 }
 
 function zipped(file_name, file_path) {
@@ -156,10 +327,6 @@ function zipped(file_name, file_path) {
 	zip.pipe(output);
 	zip.directory(file_path, '');
 	zip.finalize();
-}
-
-async function walkAsync(src, target, memory) {
-
 }
 
 async function compare(src_path, target_path, target, memory) {
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,11 @@
 const resourcepackDiv = document.getElementById('resourcepacks');
 const addButton = document.getElementById('add-file');
 const combineButton = document.getElementById('combine');
+const progressBar = document.getElementById('loader');
 const {ipcRenderer} = require('electron');
+
+let maxProgress = 0;
+let currentProgress = 0;
 
 addButton.addEventListener('click', event => {
 	ipcRenderer.send('request:add_resourcepack', 'main');
@@ -14,6 +18,46 @@ combineButton.addEventListener('click', event => {
 ipcRenderer.on('response:update_resourcepack', (event, data) => {
 	updateResourcepackList(data);
 });
+
+ipcRenderer.on('response:compile:init_length', (event, data) => {
+	enableProgressBar(data);
+});
+
+ipcRenderer.on('response:compile:update_count', (event, data) => {
+	currentProgress = data;
+	console.log(data);
+	updateProgressBar();
+});
+
+function enableProgressBar(i) {
+	progressBar.style.display = 'flex';
+	maxProgress = i;
+	currentProgress = 0;
+	updateProgressBar();
+}
+
+function disableProgressBar() {
+	progressBar.style.display = 'none';
+	maxProgress = 0;
+	currentProgress = 0;
+}
+
+function updateProgressBar() {
+	const info = progressBar.querySelector('#loader_information');
+	const outer = progressBar.querySelector('.loading_container');
+	const inner = progressBar.querySelector('.progress');
+
+	info.innerText = `${currentProgress}/${maxProgress}`;
+	
+	let outerWidth = outer.offsetWidth;
+	let innerWidth = currentProgress/maxProgress;
+
+	inner.style.width = innerWidth * outerWidth;
+
+	if (currentProgress === maxProgress) {
+		disableProgressBar();
+	}
+}
 
 function updateResourcepackList(resourcepacks) {
 	resourcepackDiv.innerHTML = '';
